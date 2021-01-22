9c8f1172f5a6a50717b4f759855c7206d683a2ee
MindaugasSak <m.sakenis@gmail.com>
2021-01-22 22:21:01 +0200

    exported all svgs for cleaner code

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as Logo } from './logo-barbora.png';
+export { default as GlutenIcon } from './Icons/gluten-free.svg';
+export { default as HeartIcon } from './Icons/heart.svg';
+export { default as TrashIcon } from './Icons/delete.svg';
+export { default as BasketIcon } from './Icons/shopping-basket.svg';
+export { default as CartIcon } from './Icons/shopping-cart.svg';
