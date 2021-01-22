e64e1fa9d8f2d523da425b08dbe568792b3a8214
MindaugasSak <m.sakenis@gmail.com>
2021-01-22 22:24:10 +0200

    products as data created

---
+++ src/utils/products.js
@@ -0,0 +1,90 @@
+const products = [
+  {
+    id: 1,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=94be4f34-a6ba-4c21-adda-0424ae059091',
+    title: 'Tirpiosios kavos gėrimas JACOBS 3 IN 1 (20 x 15,2 g), 20 vnt.',
+    price: 2.49,
+    glutenFree: true,
+  },
+  {
+    id: 2,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=4698b689-7790-4e70-b359-fe137ce27df8',
+    title: 'Majonezinis padažas WOWMAMA (mėsainių), 450 g',
+    price: 1.29,
+  },
+  {
+    id: 3,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=ae019e96-1e2f-4b59-a118-3a40a5993a95',
+    title:
+      'Ypač tyras alyvuogių aliejus COIMBRA PREMIUM (rūgšting.0,2%), 750 ml',
+    price: 5.69,
+  },
+  {
+    id: 4,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=3d45fd1b-b559-46e6-9fe9-0ede7c9f1e97',
+    title: 'Drėkinamasis kūno kremas NIVEA SOFT, 200 ml',
+    price: 3.29,
+  },
+  {
+    id: 5,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=78e26edc-eedd-417d-99be-d2459e8ff21d',
+    title: 'Plaukų šampūnas HEAD & SHOULDERS CLASSlC CLEAN, 400 ml',
+    price: 3.95,
+  },
+  {
+    id: 6,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=7752ba94-472e-4f83-bc9b-42a5ca62698b',
+    title: 'Avižų košė QUICK&TASTY su vyšniomis, 40 g',
+    price: 0.19,
+  },
+  {
+    id: 7,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=1aad60c7-a234-486d-b54f-ddd4d6bfd9b8',
+    title: 'Juodieji pomidorai KUMATO, 47-57 mm, 1 kg',
+    price: 1.99,
+  },
+  {
+    id: 8,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=c71c28cc-11f4-4c9a-a84b-705e538e847c',
+    title: 'Pievagrybiai BALTIC CHAMPIGNONS (20-50 mm, indelyje), 250 g',
+    price: 0.59,
+  },
+  {
+    id: 9,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=6b9488a7-dca2-4f95-a997-5cb5024a8454',
+    title: 'Raudonos vynuogės, 1 kg',
+    price: 2.99,
+  },
+  {
+    id: 10,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=df06ff8b-4b51-405f-bc48-a4deaeb273eb',
+    title: 'Kokosų gėrimas KOKO su vitaminais ir minelarais, 1 l',
+    price: 2.39,
+  },
+  {
+    id: 11,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=9b5ef3a4-d02c-472b-82c2-e43f3624f658',
+    title: 'Ekologiškas kefyras DOBILAS, 2,5% rieb., 900 g',
+    price: 1.19,
+  },
+  {
+    id: 12,
+    img:
+      'https://pagrindinis.barbora.lt/api/images/GetInventoryImage?id=9640db3d-1de0-4b37-b15a-651c0a99577b',
+    title: 'Ekologiška natūrali grietinė DOBILAS, 30% rieb., 330 g',
+    price: 1.51,
+  },
+];
+
+export default products;
