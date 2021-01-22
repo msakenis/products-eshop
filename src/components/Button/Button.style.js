89131cd16f0c7d38605631dae60d5277b3d78941
MindaugasSak <m.sakenis@gmail.com>
2021-01-22 22:20:34 +0200

    style changes in button

--- src/components/Button/Button.style.js
+++ src/components/Button/Button.style.js
@@ -3,15 +3,14 @@ import styled from 'styled-components';
 export const Button = styled.button`
   color: #fff;
   background-color: ${({ theme }) => theme.red.light};
-  height: 2em;
-  max-width: 10.875em;
   line-height: 1em;
   font-weight: 600;
   border-radius: 25px;
-  padding: 0.5em 1em;
+  padding: 1em;
   font-size: 0.75rem;
   width: 100%;
   border: 0;
+  margin-top: 3em;
   cursor: pointer;
 
   &&:hover {
