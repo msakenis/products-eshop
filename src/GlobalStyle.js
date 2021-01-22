ed2ed9e491a03a4ac4f7ec8df9d24426c78aaeb4
MindaugasSak <m.sakenis@gmail.com>
2021-01-22 14:06:17 +0200

    init font size and assets added

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,8 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-body {
 
+html {
+  font-size: 16px;
   font-family: 'Open Sans', sans-serif;
   background-color: ${({ theme }) => theme.background.main};
   color: ${({ theme }) => theme.grey.dark};
