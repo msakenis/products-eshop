09e46b7d1091291778d8711e0ef06c307562e1ac
MindaugasSak <m.sakenis@gmail.com>
2021-01-23 20:50:12 +0200

    switches the product style on screen width

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -4,6 +4,7 @@ import * as S from './ProductCard.style';
 import { CartContext } from '../../contexts/cart.context';
 import { Button, CartCounter } from '../';
 import { toCurrency } from '../../helpers/SharedFunctions';
+import { shape, number, string, bool, arrayOf } from 'prop-types';
 
 const ProductCard = ({ products }) => {
   const { addProduct, cartItems } = useContext(CartContext);
@@ -11,33 +12,84 @@ const ProductCard = ({ products }) => {
 
   return (
     <>
-      {products &&
-        products.map((product) => (
-          <S.AllBlockDiv key={product.id}>
-            <S.IconDiv>
-              {product.glutenFree && (
-                <S.Icon src={GlutenIcon} alt="Gluten Free Product" />
-              )}
-              <S.Icon src={HeartIcon} alt="Krepseliu ruosiniai" />
-            </S.IconDiv>
-            <S.ProductImgDiv>
-              <S.ProductImg src={product.img} alt={product.title} />
-            </S.ProductImgDiv>
-            <S.StyledLink to="/">{product.title}</S.StyledLink>
-            <S.PriceTag>{toCurrency(product.price)}</S.PriceTag>
-            <S.ButtonDiv>
-              {isInCart(product.id) ? (
-                <CartCounter product={product} />
-              ) : (
-                <Button handleClick={() => addProduct(product)}>
-                  Į krepšelį
-                </Button>
-              )}
-            </S.ButtonDiv>
-          </S.AllBlockDiv>
-        ))}
+      {products && window.innerWidth > 820
+        ? products.map((product) => (
+            <S.AllBlockDiv key={product.id}>
+              <S.IconDiv>
+                {product.glutenFree && (
+                  <S.Icon src={GlutenIcon} alt="Gluten Free Product" />
+                )}
+                <S.Icon src={HeartIcon} alt="Krepseliu ruosiniai" />
+              </S.IconDiv>
+
+              <S.ProductImgDiv>
+                <S.ProductImg src={product.img} alt={product.title} />
+              </S.ProductImgDiv>
+
+              <S.StyledLink to="/">{product.title}</S.StyledLink>
+
+              <S.PriceTag>{toCurrency(product.price)}</S.PriceTag>
+
+              <S.ButtonDiv>
+                {isInCart(product.id) ? (
+                  <CartCounter product={product} />
+                ) : (
+                  <Button handleClick={() => addProduct(product)}>
+                    Į krepšelį
+                  </Button>
+                )}
+              </S.ButtonDiv>
+            </S.AllBlockDiv>
+          ))
+        : products.map((product) => (
+            <S.AllBlockDiv key={product.id}>
+              <S.ResFlexWrapper>
+                <S.ProductImgDiv>
+                  <S.ProductImg src={product.img} alt={product.title} />
+                </S.ProductImgDiv>
+
+                <S.ResTitleBlock>
+                  <S.StyledLink to="/">{product.title}</S.StyledLink>
+
+                  {product.glutenFree && (
+                    <S.Icon src={GlutenIcon} alt="Gluten Free Product" />
+                  )}
+                </S.ResTitleBlock>
+
+                <S.IconDiv>
+                  <S.Icon src={HeartIcon} alt="Krepseliu ruosiniai" />
+                </S.IconDiv>
+              </S.ResFlexWrapper>
+
+              <S.ResFlexWrapper>
+                <S.PriceTag>{toCurrency(product.price)}</S.PriceTag>
+
+                <S.ButtonDiv>
+                  {isInCart(product.id) ? (
+                    <CartCounter product={product} />
+                  ) : (
+                    <Button handleClick={() => addProduct(product)}>
+                      Į krepšelį
+                    </Button>
+                  )}
+                </S.ButtonDiv>
+              </S.ResFlexWrapper>
+            </S.AllBlockDiv>
+          ))}
     </>
   );
+};
+
+ProductCard.propTypes = {
+  products: arrayOf(
+    shape({
+      id: number.isRequired,
+      img: string.isRequired,
+      title: string.isRequired,
+      price: number.isRequired,
+      glutenFree: bool,
+    }),
+  ),
 };
 
 export default ProductCard;
--- src/components/ProductCard/ProductCard.style.js
+++ src/components/ProductCard/ProductCard.style.js
@@ -9,12 +9,26 @@ export const AllBlockDiv = styled.div`
   box-sizing: border-box;
   height: 24em;
   border: 1px solid ${({ theme }) => theme.grey.light};
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    width: 100%;
+    height: 12em;
+    border: 0;
+    margin: 0;
+    border-bottom: 1px solid ${({ theme }) => theme.grey.light};
+  }
 `;
 
 export const IconDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: end;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    width: 10%;
+    display: block;
+    text-align: right;
+  }
 `;
 
 export const Icon = styled.img`
@@ -22,6 +36,7 @@ export const Icon = styled.img`
   padding-left: 0.3em;
   filter: invert(18%) sepia(0%) saturate(0%) hue-rotate(143deg) brightness(96%)
     contrast(81%);
+
   &&:last-child {
     &&:hover {
       filter: invert(60%) sepia(94%) saturate(6696%) hue-rotate(339deg)
@@ -29,18 +44,31 @@ export const Icon = styled.img`
       cursor: pointer;
     }
   }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    max-width: 1.5em;
+    padding-top: 0.3em;
+  }
 `;
 
 export const ProductImg = styled.img`
   object-fit: contain;
   width: 100%;
   height: 9.25em;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    height: 100%;
+  }
 `;
 
 export const ProductImgDiv = styled.div`
   width: 100%;
   box-sizing: border-box;
   text-align: center;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    width: 25%;
+  }
 `;
 
 export const StyledLink = styled(Link)`
@@ -52,8 +80,14 @@ export const StyledLink = styled(Link)`
   line-height: 1.2;
   height: 5em;
   margin: 0.3em 0;
+
   &&:hover {
     color: ${({ theme }) => theme.red.light};
+  }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    font-size: 1.2rem;
+    text-align: left;
   }
 `;
 
@@ -63,6 +97,29 @@ export const PriceTag = styled.p`
   font-weight: 600;
   text-align: center;
   margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    margin: auto 0;
+    padding-top: 1.2em;
+    padding-left: 0.6em;
+  }
 `;
 
-export const ButtonDiv = styled.div``;
+export const ButtonDiv = styled.div`
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    width: 50%;
+  }
+`;
+
+export const ResTitleBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 75%;
+`;
+
+export const ResFlexWrapper = styled.div`
+  display: flex;
+  height: 50%;
+  width: 100%;
+  justify-content: space-between;
+`;
