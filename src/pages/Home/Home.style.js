aed0f0dc2a9ae4edfe90a44cf1b7d2bf16f6ffb0
MindaugasSak <m.sakenis@gmail.com>
2021-01-23 20:50:32 +0200

    responsive

--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -4,10 +4,17 @@ export const MainWrapper = styled.div`
   max-width: 84em;
   margin: 0 auto;
   display: flex;
+  position: relative;
 `;
 
 export const ProductsFlex = styled.div`
   display: flex;
   flex-wrap: wrap;
+  align-items: flex-start;
   width: 75%;
+  height: 100%;
+
+  @media (max-width: ${({ theme }) => theme.tablet}) {
+    width: 100%;
+  }
 `;
