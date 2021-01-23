9970a4e43d86a09f18ac03febbc3b11c4b4d3278
MindaugasSak <m.sakenis@gmail.com>
2021-01-23 20:47:24 +0200

    header responsive

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -3,10 +3,18 @@ import styled from 'styled-components';
 export const Logo = styled.img`
   height: 3em;
   padding: 1em;
+
+  @media (max-width: ${({ theme }) => theme.tablet}) {
+    height: 2em;
+  }
 `;
 export const Header = styled.header`
   background-color: #fff;
   border-bottom: 1px solid ${({ theme }) => theme.grey.light};
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    border-bottom: 2px solid #999;
+  }
 `;
 
 export const Wrapper = styled.div`
@@ -19,4 +27,12 @@ export const Wrapper = styled.div`
 export const CartButtonDiv = styled.div`
   width: 25%;
   margin: auto 0;
+
+  @media (max-width: ${({ theme }) => theme.tablet}) {
+    width: 13%;
+  }
+
+  @media (max-width: ${({ theme }) => theme.mobile}) {
+    width: 25%;
+  }
 `;
