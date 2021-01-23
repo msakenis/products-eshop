dd5ec16ef78d9c87bd74050a098f9798624e1a7c
MindaugasSak <m.sakenis@gmail.com>
2021-01-23 20:52:10 +0200

    props checking

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import * as S from './Button.style';
+import { string, func } from 'prop-types';
 
 const Button = ({ children, handleClick }) => {
   return <S.Button onClick={handleClick}>{children}</S.Button>;
+};
+
+Button.propTypes = {
+  handleClick: func.isRequired,
+  children: string.isRequired,
 };
 
 export default Button;
