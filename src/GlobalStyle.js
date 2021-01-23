a634cf6b6372513a243523ad4eee52aaf954b3e6
MindaugasSak <m.sakenis@gmail.com>
2021-01-23 20:48:54 +0200

    global responsivness

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -7,7 +7,9 @@ html {
   font-family: 'Open Sans', sans-serif;
   background-color: ${({ theme }) => theme.background.main};
   color: ${({ theme }) => theme.grey.dark};
-
+  @media (max-width: ${({ theme }) => theme.tablet}) {
+    font-size: 15px;
+  }
 }
 `;
 export default GlobalStyle;
